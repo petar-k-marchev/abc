782c1738b09a21e057d67a5a0b6319bf52d8ad15
progress\marchev <petar.marchev@progress.com>
2020-05-14 18:44:48 +0300

    Put a bag in the visual tree.

--- Abc/Abc/VisualTree/AbcVisualTree.cs
+++ Abc/Abc/VisualTree/AbcVisualTree.cs
@@ -8,11 +8,32 @@ namespace Abc
     {
         protected readonly Dictionary<Type, Func<IAbcVisual>> visualCreator = new Dictionary<Type, Func<IAbcVisual>>();
 
+        private AbcBag bag;
+
         internal virtual IAbcVisual CreateVisual(Type abcVisualType)
         {
             Func<IAbcVisual> creator = visualCreator[abcVisualType];
             IAbcVisual visual = creator();
             return visual;
+        }
+
+        internal AbcBag Bag
+        {
+            get
+            {
+                if (this.bag == null)
+                {
+                    lock (this)
+                    {
+                        if (this.bag == null)
+                        {
+                            this.bag = new AbcBag();
+                        }
+                    }
+                }
+
+                return this.bag;
+            }
         }
     }
 }
